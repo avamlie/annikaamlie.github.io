4c93175f5da41b49a954018915b3e767c9607189
Annika Amlie <aamlie.01@gmail.com>
2024-05-27 18:46:54 -0400

    Updates

--- precache-manifest.4fe5be0389463e0868da921943b166b9.js
+++ precache-manifest.4fe5be0389463e0868da921943b166b9.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "fb0f75c6775d82e6e1a8",
+    "revision": "d8c47a1a38babf58b7c0",
     "url": "/static/css/main.2a5280da.chunk.css"
   },
   {
-    "revision": "fb0f75c6775d82e6e1a8",
-    "url": "/static/js/main.fb0f75c6.chunk.js"
+    "revision": "d8c47a1a38babf58b7c0",
+    "url": "/static/js/main.d8c47a1a.chunk.js"
   },
   {
     "revision": "fdfcfda2d9b1bf31db52",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/static/js/2.d6cbbdcb.chunk.js"
   },
   {
-    "revision": "ed0b97fc5341640e4f503473cb120487",
+    "revision": "f1f8d10ef7aa75806e6d3d985cf9f278",
     "url": "/index.html"
   }
 ];
